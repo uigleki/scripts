ae13fed27f210b641f4db9f92791f9b414b74f17
游元 <rraayy246@gmail.com>
2022-09-03 16:54:22 +0000

    Update win.ps1

--- sh/win.ps1
+++ sh/win.ps1
@@ -1,3 +1,6 @@
+# 仓库地址
+$url = "https://gitlab.com/glek/scripts/raw/main/win"
+
 # 下载常用软件
 winget install 7zip.7zip
 winget install Klocman.BulkCrapUninstaller
@@ -5,9 +8,16 @@ winget install Microsoft.PowerToys
 winget install Mozilla.Firefox
 
 # 小鹤双拼 键位
-Invoke-WebRequest https://raw.githubusercontent.com/2015WUJI01/xhup-for-win10/master/xhup.reg -OutFile "xhup.reg"
+Invoke-WebRequest "$url/xhup.reg" -OutFile "xhup.reg"
 regedit /s "xhup.reg"
 Remove-Item "xhup.reg"
 
 # UTC 时间 (需要管理员权限)
 Reg add HKLM\SYSTEM\CurrentControlSet\Control\TimeZoneInformation /v RealTimeIsUniversal /t REG_DWORD /d 1
+
+# 键盘映射
+$dest = "$env:LOCALAPPDATA/Microsoft/PowerToys"
+mkdir -p "$dest/Keyboard Manager"
+Invoke-WebRequest "$url/powertoys/settings.json" -OutFile "$dest/settings.json"
+Invoke-WebRequest "$url/powertoys/keyboard/settings.json" -OutFile "$dest/Keyboard Manager/settings.json"
+Invoke-WebRequest "$url/powertoys/keyboard/default.json" -OutFile "$dest/Keyboard Manager/default.json"
