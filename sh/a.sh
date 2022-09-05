c28e36700b9ae77c306417001ffb78469862b821
ray <rraayy246@gmail.com>
2022-09-05 14:05:42 +0800

    rsync

--- sh/a.sh
+++ sh/a.sh
@@ -1,5 +1,6 @@
-rsync_argu=(-rP --delete --inplace --no-whole-file)
-rsync -n ${rsync_argu[@]} "$@"
+# rsync 以修改日期和文件大小判断是不是同一个文件
+rsync_argu=(--delete --inplace --no-whole-file --partial --progress --recursive --times --update)
+rsync --dry-run ${rsync_argu[@]} "$@"
 echo -ne "are you sure? "
 read sure
 if [ "$sure" = '' -o "$sure" = 'y' ]; then
