5b2c4173c17fc657dda6772498868cae0e7f7b26
ray <rraayy246@gmail.com>
2022-09-13 17:57:36 +0800

    a

--- sh/a.sh
+++ sh/a.sh
@@ -1,5 +1,5 @@
 # rsync 以修改日期和文件大小判断是不是同一个文件
-rsync_argu=(--delete --inplace --no-whole-file --partial --progress --recursive --times --update)
+rsync_argu=(--delete --inplace --no-whole-file --partial --progress --recursive --times)
 rsync --dry-run ${rsync_argu[@]} "$@"
 echo -ne "are you sure? "
 read sure
