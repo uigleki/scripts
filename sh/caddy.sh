3d873390a5b61c8164e9960229e37eb4f87b3c56
ray <rraayy246@gmail.com>
2022-10-01 19:35:48 +0800

    caddy to podman

--- sh/caddy.sh
+++ sh/caddy.sh
@@ -1,5 +1,9 @@
-sudo pacman -S --noconfirm caddy
+# 允许用户使用 80 端口
+echo 'net.ipv4.ip_unprivileged_port_start = 80' | sudo tee /etc/sysctl.d/podman.conf
+sudo sysctl $(cat /etc/sysctl.d/podman.conf | sed 's/ //g')
 
-# 增加 UDP 缓冲区大小
-echo "net.core.rmem_max = 2500000" | sudo tee /etc/sysctl.d/rmem_max.conf
-sudo sysctl $(cat /etc/sysctl.d/rmem_max.conf | sed 's/ //g')
+podman run -d --name caddy \
+       -p 80:80 -p 443:443 -p 443:443/udp \
+       -v /srv/http:/srv/http \
+       -v /etc/caddy:/etc/caddy \
+       caddy
