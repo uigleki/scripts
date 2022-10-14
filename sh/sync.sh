9a611187042cc40a1c86dbe44346df763cfdcaa1
ray <rraayy246@gmail.com>
2022-10-14 23:06:12 +0800

    rsync exclude

--- sh/sync.sh
+++ sh/sync.sh
@@ -1,8 +1,17 @@
 # rsync 以修改日期和文件大小判断是不是同一个文件
-rsync_argu=(--delete --inplace --no-whole-file --partial --progress --recursive --times)
+exclude_list=$HOME/exclude.list
+rsync_argu=(--delete --exclude-from=$exclude_list --inplace --no-whole-file --partial --progress --recursive --times)
+
+cat << EOF > $exclude_list
+.owncloudsync.log
+.sync*
+EOF
+
 rsync --dry-run ${rsync_argu[@]} "$@"
 echo -ne "are you sure? "
 read sure
 if [ "$sure" = '' -o "$sure" = 'y' ]; then
-  rsync ${rsync_argu[@]} "$@"
+    rsync ${rsync_argu[@]} "$@"
 fi
+
+rm $exclude_list
