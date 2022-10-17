d600fe64dbebbab52de917bf6a2c66741fd31dc4
ray <rraayy246@gmail.com>
2022-10-18 00:56:46 +0800

    a

--- shell/common/00-set-var.sh
+++ shell/common/00-set-var.sh
@@ -2,5 +2,5 @@ config_name=dotfiles
 config_dir=$HOME/$config_name
 config_url=https://gitlab.com/uigleki/$config_name.git
 
-user_email=rraayy246@gmail.com
-user_name=ray
+git_user_email=rraayy246@gmail.com
+git_user_name=ray
--- shell/common/01-clone-config-repo.sh
+++ shell/common/01-clone-config-repo.sh
@@ -2,5 +2,5 @@ git clone --depth=1 $config_url $config_dir
 
 git config --global credential.helper store
 git config --global pull.rebase false
-git config --global user.email $user_email
-git config --global user.name $user_name
+git config --global user.email $git_user_email
+git config --global user.name $git_user_name
