a3d90397a52005d775c9516cc8914ab2f251ce21
ray <rraayy246@gmail.com>
2022-10-18 12:55:09 +0800

    a

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-setup_dir=common
+setup_dir=$(dirname $0)
+setup_name=common
 
 if [ -n "$1" ]; then
-    setup_dir=$1
+    setup_name=$1
 fi
 
-for file in $setup_dir/*.sh; do
+for file in $setup_dir/$setup_name/*.sh; do
     source $file
 done
