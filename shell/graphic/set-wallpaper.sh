9c345768ac3cbf9d0e84b31180ea410d00bf1d8d
ray <rraayy246@gmail.com>
2022-10-18 14:43:54 +0800

    a

--- shell/graphic/set-wallpaper.sh
+++ shell/graphic/set-wallpaper.sh
@@ -1,13 +1,13 @@
 tmp() {
     local wallpaper_name=ArchLinux.png
     local wallpaper_file=$config_dir/img/$wallpaper_name
-    local kde_wallpaper_file=$HOME/dilnu/jmaji/pixra/bimple/$wallpaper_name
-    local sddm_wallpaper_file=/usr/share/sddm/themes/breeze/$wallpaper_name
+    local kde_wallpaper_dir=$HOME/dilnu/jmaji/pixra/bimple
+    local sddm_theme_dir=/usr/share/sddm/themes/breeze
 
-    mkdir -p $(dirname $wallpaper_dir)
-    rsync -t $wallpaper_file $kde_wallpaper_file
+    mkdir -p $kde_wallpaper_dir
+    rsync -t $wallpaper_file $kde_wallpaper_dir
 
-    sudo rsync -t $wallpaper_file $sddm_wallpaper_file
+    sudo rsync -t $wallpaper_file $sddm_theme_dir
     cat << EOF | sudo tee $sddm_theme_dir/theme.conf.user
 [General]
 background=${wallpaper_name}
