07ac3933be0645e4bc8fe2a4135040537d71d3e2
ray <rraayy246@gmail.com>
2022-10-18 13:49:29 +0800

    a

--- shell/common/console-keymap.sh
+++ shell/common/console-keymap.sh
@@ -1,7 +1,5 @@
 sudo mkdir -p /usr/local/share/kbd/keymaps
 cat << EOF | sudo tee /usr/local/share/kbd/keymaps/personal.map
-keycode 29 = Alt
-keycode 56 = Control
 keycode 58 = Home
 EOF
 echo 'KEYMAP=/usr/local/share/kbd/keymaps/personal.map' | sudo tee /etc/vconsole.conf
