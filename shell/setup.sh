a2e26f73ae29b89b778420fc3f2acc8c5f808c65
ray <rraayy246@gmail.com>
2022-10-18 15:09:03 +0800

    a

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# 用户变量
+config_name=dotfiles
+config_dir=$HOME/$config_name
+config_repo=https://gitlab.com/uigleki/$config_name.git
+
+git_user_email=rraayy246@gmail.com
+git_user_name=ray
+
+# 执行
 setup_dir=$(dirname $0)
 setup_name=${1:-common}
 
