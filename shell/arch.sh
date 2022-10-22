b996ddbf0a27e886e18f03d4a017c7750781cbe2
ray <rraayy246@gmail.com>
2022-10-22 17:51:10 +0800

    如果在小内存使用 oomd，会导致重要进程被终止，oomd 以内存占用杀后台

--- shell/arch.sh
+++ shell/arch.sh
@@ -628,12 +628,12 @@ install_bootloader() {
 set_auto_start() {
     local mask_list=(systemd-resolved)
     local disable_list=(systemd-timesyncd)
-    local enable_list=(auditd apparmor btrfs-scrub@-.timer chronyd dnscrypt-proxy fcron firewalld fstrim.timer grub-btrfs.path paccache.timer pkgstats.timer sshd systemd-oomd)
+    local enable_list=(auditd apparmor btrfs-scrub@-.timer chronyd dnscrypt-proxy fcron firewalld fstrim.timer grub-btrfs.path paccache.timer pkgstats.timer sshd)
 
     if [ "$use_gui" = 1 ]; then
         # dhcpcd 和 NetworkManager 不能同时启动
         disable_list+=(dhcpcd)
-        enable_list+=(bluetooth libvirtd NetworkManager reflector.timer sddm)
+        enable_list+=(bluetooth libvirtd NetworkManager reflector.timer sddm systemd-oomd)
     else
         enable_list+=(dhcpcd)
     fi
