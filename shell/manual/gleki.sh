26b29fe24ed44b24c3c057243382d6fea4b04015
ray <rraayy246@gmail.com>
2022-10-30 13:11:06 +0800

    snyapse

--- shell/manual/gleki.sh
+++ shell/manual/gleki.sh
@@ -10,27 +10,60 @@ var_read() {
     echo "${var_name}=${!var_name}"
 }
 
-sudo chown -R $USER: /srv/http
-cd /srv/http
-mkdir mnt
-git clone --depth=1 $gleki_repo
+clone_repo() {
+    sudo chown -R $USER: /srv/http
+    cd /srv/http
+    mkdir mnt
+    git clone --depth=1 $gleki_repo
+}
 
-cd gleki
-gocryptfs cry ../mnt
-rsync -rt ../mnt/etc ..
-fusermount -u ../mnt
 
-var_read admin
-var_read cloudpass
-sed -i "s/admin/${admin}/" pod/cloud.yaml
-sed -i "s/cloudpass/${cloudpass}/" pod/cloud.yaml
+copy_config() {
+    cd /srv/http/gleki
+    gocryptfs cry ../mnt
+    rsync -rt ../mnt/etc ..
+    fusermount -u ../mnt
+}
 
-cd pod
-podman play kube cloud.yaml
+change_cloud_pass() {
+    var_read admin
+    var_read cloudpass
+    sed -i "s/admin/${admin}/" pod/cloud.yaml
+    sed -i "s/cloudpass/${cloudpass}/" pod/cloud.yaml
+}
 
-mkdir -p ~/.config/systemd/user
-cd ~/.config/systemd/user
-# 用户实例自动启动，让用户进程跟会话分离
-sudo loginctl enable-linger $USER
-podman generate systemd -f -n cloud
-systemctl --user enable pod-cloud
+set_synapse() {
+    cd /srv/http/gleki
+    podman run -it --rm \
+           -v synapse:/data \
+           -v ../etc/synapse:/data/config \
+           -e SYNAPSE_CONFIG_DIR=/data/config \
+           -e SYNAPSE_SERVER_NAME=gleki.com \
+           -e SYNAPSE_REPORT_STATS=yes \
+           matrixdotorg/synapse generate
+}
+
+run_pod() {
+    cd /srv/http/gleki/pod
+    podman play kube cloud.yaml
+}
+
+auto_start() {
+    mkdir -p ~/.config/systemd/user
+    cd ~/.config/systemd/user
+    # 用户实例自动启动，让用户进程跟会话分离
+    sudo loginctl enable-linger $USER
+    podman generate systemd -f -n cloud
+    systemctl --user enable pod-cloud
+}
+
+main() {
+    clone_repo
+    copy_config
+    change_cloud_pass
+    set_synapse
+    run_pod
+    auto_start
+}
+
+main
