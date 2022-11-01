923e922c0ba56bb2d638a3731ce87402e5fd6cf7
ray <rraayy246@gmail.com>
2022-11-01 20:40:01 +0800

    a

--- shell/server/tcp_bbr.sh
+++ shell/server/tcp_bbr.sh
@@ -5,7 +5,9 @@ set -eo pipefail
 echo "tcp_bbr" | sudo tee -a /etc/modules-load.d/modules.conf
 
 # 将默认的拥塞算法设置为 bbr
-echo "net.core.default_qdisc = cake" | sudo tee /etc/sysctl.d/tcp_bbr.conf
-echo "net.ipv4.tcp_congestion_control = bbr" | sudo tee -a /etc/sysctl.d/tcp_bbr.conf
+cat << EOF | sudo tee /etc/sysctl.d/tcp_bbr.conf
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
+EOF
 
 echo 'need reboot'
