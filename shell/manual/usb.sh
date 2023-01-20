35950c97a7137142faf58a7766884e17b47d7ca3
ray <rraayy246@gmail.com>
2023-01-20 18:16:16 +0800

    usb sync

--- shell/manual/usb.sh
+++ shell/manual/usb.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-usb="$1"
+test_file=$HOME/dilnu/jmaji/zgike
 sync=$(dirname $0)/sync.sh
-bash $sync --exclude=Downloads/iso ~/Downloads ~/dilnu $usb/btrfs
-bash $sync ~/Downloads/iso/ $usb/Ventoy
+
+udisksctl mount -b /dev/sda1
+udisksctl mount -b /dev/sda3
+
+if [ -e $test_file ]; then
+    bash $sync --exclude=Downloads/iso ~/Downloads ~/dilnu $usb/btrfs
+    bash $sync ~/Downloads/iso/ $usb/Ventoy
+else
+    bash $sync $usb/btrfs/Downloads $usb/btrfs/dilnu ~
+    bash $sync $usb/Ventoy/ ~/Downloads/iso
+fi
+
+udisksctl unmount -b /dev/sda1
+udisksctl unmount -b /dev/sda3
+udisksctl power-off -b /dev/sda
