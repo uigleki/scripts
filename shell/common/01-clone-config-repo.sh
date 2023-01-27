eb51a3c465ada206b3d3a93a890bf1a6f4157d28
ray <rraayy246@gmail.com>
2023-01-27 18:26:25 +0800

    a

--- shell/common/01-clone-config-repo.sh
+++ shell/common/01-clone-config-repo.sh
@@ -1,6 +1,1 @@
 git clone --depth=1 $config_repo $config_dir
-
-git config --global credential.helper store
-git config --global pull.rebase false
-git config --global user.email $git_user_email
-git config --global user.name $git_user_name
